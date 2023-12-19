7422618cdd31cceed0c508ea1e31f7422e458618
Christopher <chihaicristian.contact@gmail.com>
2023-12-19 14:31:24 +0200

    Implemented a database connection component and applied singleton pattern to share the same connection across all services

---
+++ Configuration/DbConnection.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace MedAppointments.Configuration
+{
+    public sealed class DbConnection
+    {
+        private static DbConnection _instance;
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string user;
+        private string password;
+        private string port;
+        private string connectionString;
+        private string sslMode;
+
+        private DbConnection()
+        {
+            server = "localhost";
+            database = "appointments";
+            user = "root";
+            password = "password";
+            port = "3306";
+            sslMode = "none";
+            connectionString = String.Format("server={0};port={1};user id={2}; password={3}; database={4}; SslMode={5}", server, port, user, password, database, sslMode);
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+        public static DbConnection GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new DbConnection();
+            }
+            return _instance;
+        }
+
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        MessageBox.Show("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+    }
+}
--- Forms/MainForm.cs
+++ Forms/MainForm.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Common;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MedAppointments.Configuration;
 
 namespace MedAppointments
 {
